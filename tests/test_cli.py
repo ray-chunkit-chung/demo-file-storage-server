591631565f45c2db76c6e8811d06b8440dd6309e
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2023-02-09 15:26:40 +0900

    gitignore

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,13 +6,23 @@ runner = CliRunner()
 
 
 def test_cli_version():
-    result = runner.invoke(app, ["--version"])
+    result = runner.invoke(app, ['--version'])
     assert result.exit_code == 0
-    assert f"{APP_NAME} v{VERSION}\n" in result.stdout
+    assert f'{APP_NAME} v{VERSION}\n' in result.stdout
 
 
-def test_cli_upload_file():
-    pass
+def test_cli_upload_happy():
+    """ Expect a File not found message"""
+    result = runner.invoke(app, ['upload-file', 'this_file_do_not_exist.txt'])
+    assert result.exit_code == 0
+    assert f'File not found' in result.stdout
+
+
+def test_cli_upload_unhappy_file_not_exist():
+    """ Expect a File not found message"""
+    result = runner.invoke(app, ['upload-file', 'this_file_do_not_exist.txt'])
+    assert result.exit_code == 0
+    assert f'File not found' in result.stdout
 
 
 def test_cli_delete_file():
